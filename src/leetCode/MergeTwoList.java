a433f25d01d0b2e63d0d33cb435b962439936bf9
Nirali Rathod <59702795+Niraaaliii@users.noreply.github.com>
2023-05-31 15:26:37 -0500

    Added new package

---
+++ src/leetCode/MergeTwoList.java
@@ -0,0 +1,30 @@
+package leetCode;
+
+public class MergeTwoList {
+
+	public static void main(String[] args) {
+
+	}
+
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+		return null;
+	}
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+}
