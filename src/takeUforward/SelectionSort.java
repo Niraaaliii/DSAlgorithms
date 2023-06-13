edecac7ea804bf5540f03e24b0d433b7ed9c5fe3
Nirali Rathod <59702795+Niraaaliii@users.noreply.github.com>
2023-06-13 10:25:36 -0500

    revised program
    changed file

--- src/takeUforward/SelectionSort.java
+++ src/takeUforward/SelectionSort.java
@@ -29,18 +29,23 @@ public class SelectionSort {
 	}
 	
 	public static void sortArray(int[] array){
-		int tmp = 0;
-		for(int i=0;i<=array.length-1;i++) {
+		
+		for (int i = 0; i < array.length - 1; i++) {
 			
-			for(int j=i+1;j<array.length;j++) {
-				
-				if(array[j]<array[i]) {
-					tmp = array[j];
-					array[j] = array[i];
-					array[i] = tmp;
-				}
-			}
-		}
+            int x = i;
+            
+            for (int j = i + 1; j < array.length; j++) {
+            	
+                if (array[j] < array[x]) {
+                	
+                    x = j;
+                }
+            }
+            
+            int temp = array[x];
+            array[x] = array[i];
+            array[i] = temp;
+        }
         
 	}
 
