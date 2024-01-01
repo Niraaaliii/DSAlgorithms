af4f80b947280f8107a9e95925bef5937ea3db46
Nirali Rathod <59702795+Niraaaliii@users.noreply.github.com>
2023-12-31 21:24:30 -0600

    update

---
+++ src/leetCode/IsPalindrome.java
@@ -0,0 +1,40 @@
+package leetCode;
+
+//Example 1:
+//
+//Input: s = "A man, a plan, a canal: Panama"
+//Output: true
+//Explanation: "amanaplanacanalpanama" is a palindrome.
+//Example 2:
+//
+//Input: s = "race a car"
+//Output: false
+//Explanation: "raceacar" is not a palindrome.
+
+public class IsPalindrome {
+
+	static boolean isPalindrome(String s) {
+
+		String filteredString = s.replaceAll("[^a-zA-Z0-9]+", "").toLowerCase();
+		//System.out.println(filteredString);
+
+		int left = 0;
+		int right = filteredString.length() - 1;
+
+		while (left < right) {
+			if (filteredString.charAt(left) != filteredString.charAt(right)) {
+				return false;
+			}
+			left++;
+			right--;
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+		System.out.println(isPalindrome("race a car"));
+	}
+
+}
