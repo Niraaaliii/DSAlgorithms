ecb2ca44c9e019a310482291f837a92654578a25
Nirali Rathod <59702795+Niraaaliii@users.noreply.github.com>
2023-12-31 23:45:07 -0600

    update

--- src/neetcode/MaxProfit.java
+++ src/neetcode/MaxProfit.java
@@ -1,6 +1,5 @@
 package neetcode;
 
-import java.util.*;
 //Input: prices = [7,1,5,3,6,4]
 //Output: 5
 //Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
@@ -9,20 +8,6 @@ import java.util.*;
 public class MaxProfit {
 
 	static int maxProfit(int[] prices) {
-//		int lsf = Integer.MAX_VALUE; // least so far
-//		int op = 0; // overall profit
-//		int pist = 0; // price if sold today
-//
-//		for (int i = 0; i < prices.length; i++) {
-//			if (prices[i] < lsf) {
-//				lsf = prices[i];
-//			}
-//			pist = prices[i] - lsf;
-//			if (op < pist) {
-//				op = pist;
-//			}
-//		}
-//		return op;
 
 		int max_profit = Integer.MIN_VALUE;
 		int buy_price = Integer.MAX_VALUE;
