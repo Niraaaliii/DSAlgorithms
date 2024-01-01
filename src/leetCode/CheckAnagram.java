3fe58644e98bb0bdc943b2d15b884225dc4cf1a3
Nirali Rathod <59702795+Niraaaliii@users.noreply.github.com>
2023-12-31 20:14:29 -0600

    31/12/23

--- src/leetCode/CheckAnagram.java
+++ src/leetCode/CheckAnagram.java
@@ -7,24 +7,27 @@ public class CheckAnagram {
 		
 		if((s.length() != t.length()) || (s.length() < 0 || t.length() < 0)) return false;
 							
-		HashMap<Character,Integer> smap=new HashMap<>();
-        
-        for(int i=0;i<s.length();i++){
-        	
-            smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0)+1);
-            smap.put(t.charAt(i),smap.getOrDefault(t.charAt(i),0)-1);
-        }
-        
-        for(char c:smap.keySet())
-            if(smap.get(c)!=0) 
-            	return false;
-        
-        
-        return true;
+		HashMap<Character,Integer> sMap = new HashMap<Character,Integer>();
+		HashMap<Character,Integer> tMap = new HashMap<Character,Integer>();
 		
+		//populate frequency of character
+		for(char c : s.toCharArray() ) {
+			sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+		}
+		
+		for(char x : t.toCharArray()) {
+			tMap.put(x, tMap.getOrDefault(x, 0) + 1);
+		}
+		
+		if(sMap.equals(tMap)) {
+			return true;
+		}
+		else {
+			return false; 
+		}
 	}
 
 	public static void main(String[] args) {
-		System.out.println(isAnagram("aacc", "ccac"));
+		System.out.println(isAnagram("rat", "tar"));
 	}
 }
---
+++ src/leetCode/TwoSum.java
@@ -0,0 +1,41 @@
+package leetCode;
+import java.util.*;
+
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+public class TwoSum {
+
+	static int[] twoSum(int[] nums, int target) {
+		
+		HashMap<Integer,Integer> map = new HashMap<>();
+		
+		for(int i = 0;i<nums.length;i++) {
+			map.put(nums[i], i);
+		}
+
+		for(int i = 0;i<nums.length;i++) {
+			int complement = target - nums[i];
+			if(map.containsKey(complement) && map.get(complement) != i) {
+				return new int[]{i, map.get(complement)};
+			}
+		}
+		
+		return new int[] {}; 
+	}
+
+	public static void main(String[] args) {
+
+		int[] n = { 2, 4, 11, 3 };
+		int target = 6;
+
+		int[] ans = twoSum(n, target);
+
+		for (int i : ans) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
